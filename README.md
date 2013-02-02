a5d138c5df78b7c75a1ccf7541dd45bd40e58338
Chris Ferdinandi <hello@gomakethings.com>
2013-02-01 22:24:30 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TITLE - SUBTITLE
+DESCRIPTION
+
+## How It Works
+Getting started with TITLE is really easy. [View the online tutorial](#) or dig through the example.html file.
+
+## Changelog
+* DATE
+  * Initial release.
+
+## License
+TITLE is licensed under [WTFPL](http://www.wtfpl.net/).
---
+++ kraken.js
@@ -0,0 +1,10 @@
+/* =============================================================
+ * js-accessibility.js v1.0.0
+ * Adds .js class to <body> for progressive enhancement.
+ * Script by Chris Ferdinandi - http://gomakethings.com
+ * Licensed under WTFPL - http://www.wtfpl.net
+ * ============================================================= */
+
+$(function () {
+    $('body').addClass('js'); // On page load, add the .js class to the <body> element.
+});
