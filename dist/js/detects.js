b098bcc0fa94db7228057a90bb07859b657771ee
cferdinandi <hello@gomakethings.com>
2016-01-05 16:38:56 -0500

    v7.1.1 Updated gulp dependencies

--- dist/js/detects.js
+++ dist/js/detects.js
@@ -1,6 +1,6 @@
 /*!
- * Kraken v7.1.0: A lightweight front-end boilerplate
- * (c) 2015 Chris Ferdinandi
+ * Kraken v7.1.1: A lightweight front-end boilerplate
+ * (c) 2016 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/kraken
  */
