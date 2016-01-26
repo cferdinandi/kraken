6a3f75cb6c331aa8819a351ef6a1167e32cdae26
cferdinandi <hello@gomakethings.com>
2016-01-26 15:52:58 -0800

    v7.3.0 Added tap delay iOS fix

--- docs/dist/js/main.js
+++ docs/dist/js/main.js
@@ -1,5 +1,5 @@
 /*!
- * Kraken v7.2.0: A lightweight front-end boilerplate
+ * Kraken v7.3.0: A lightweight front-end boilerplate
  * (c) 2016 Chris Ferdinandi
  * MIT License
  * http://github.com/cferdinandi/kraken
